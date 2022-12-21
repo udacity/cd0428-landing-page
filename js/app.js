90199533e654c7c8439805250a2444a2a78e27df
mohamed elmasry <mohamedelmasery12@gmail.com>
2022-12-21 22:49:04 +0200

    task 1

--- js/app.js
+++ js/app.js
@@ -22,6 +22,10 @@
  * Define Global Variables
  * 
 */
+// varible to get the nav by id
+const nav=document.getElementById('navbar__list');
+// varible contains all sections
+const sections=document.querySelectorAll('section');
 
 
 /**
@@ -39,12 +43,65 @@
 */
 
 // build the nav
+const navBuild = () => {
+// sections loop
+    var n = '';
+    
+    sections.forEach(section => {
 
+        const secid = section.id;
+        const secnav = section.dataset.nav;
+
+        n = n +  `<li><a class="menu__link" href="#${secid}">${secnav}</a></li>`;
+
+    });
+  
+    nav.innerHTML = n;
+
+
+};
+
+navBuild();
 
 // Add class 'active' to section when near top of viewport
 
 
-// Scroll to anchor ID using scrollTO event
+const specifications = (section) => {
+    return Math.floor(section.getBoundingClientRect().top);
+};
+
+// delete active class
+const deleteActive = (section) => {
+    section.classList.remove('your-active-class');
+};
+// add activee class
+const addActiveClass = (j, section) => {
+    if(j){
+        section.classList.add('your-active-class');
+
+        
+   
+    };
+};
+
+//fuction to make section you in active 
+
+const activateSection = () => {
+    sections.forEach(section => {
+        const y = specifications(section);
+
+        xv = () => y < 150 && y >= -150;
+
+        deleteActive(section);
+        addActiveClass(xv(),section);
+    });
+};
+
+window.addEventListener('scroll' ,activateSection);
+
+
+
+
 
 
 /**
