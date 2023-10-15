a9db7385d745353556ff1b12ad2803d8969869e9
abdulllaha <abdullah.aw44@gmail.com>
2023-10-15 18:59:05 +0300

    feat: making the web site responsive

--- README.md
+++ README.md
@@ -1,9 +1,31 @@
-# Landing Page Project
+# Landing Page
 
-## Instructions
+## Description
 
-The starter project has some HTML and CSS styling to display a static version of the Landing Page project. You'll need to convert this project from a static project to an interactive one. This will require modifying the HTML and CSS files, but primarily the JavaScript file.
+this is udacity javascript project
+the project was static and the requirement is to make it responsive
+through javascript.
+# Demo
 
-To get started, open `js/app.js` and start building out the app's functionality
+# Table of Contents
+- [Landing Page](#landing-page)
+- [Demo](#demo)
+- [Table of Contents](#table-of-contents)
+- [Development](#development)
+- [Contribute](#contribute)
+- [License](#license)
 
-For specific, detailed instructions, look at the project instructions in the Udacity Classroom.
+# Development
+javascript ES6, HTML5, CSS 
+
+```shell
+command git clone https://github.com/abdulllaha/landing-page.git
+```
+
+
+# Resources
+Resources I used to develope the project
+[MDN](https://developer.mozilla.org/en-US/docs/Web/API/Element/scrollIntoView): scrollIntoView()
+[W3schools](https://www.w3schools.com/tags/att_script_defer.asp): downloading script in parallel with the page
+[Stackoverflow](https://stackoverflow.com/questions/10693845/what-do-queryselectorall-and-getelementsby-methods-return): add scrollIntoView to html collection element
+
--- js/app.js
+++ js/app.js
@@ -1,62 +1,59 @@
-/**
- * 
- * Manipulating the DOM exercise.
- * Exercise programmatically builds navigation,
- * scrolls to anchors from navigation,
- * and highlights section in viewport upon scrolling.
- * 
- * Dependencies: None
- * 
- * JS Version: ES2015/ES6
- * 
- * JS Standard: ESlint
- * 
-*/
+//declare global variables.
+const ul = document.getElementById('navbar__list');
+const fragment = document.createDocumentFragment();
+const sectionCollection = document.getElementsByTagName('section');
+const sections = Array.from(sectionCollection);
+const navList = [
+    'section 1',
+    'section 2',
+    'section 3',
+    'section 4',
+  ];
 
-/**
- * Comments should be present at the beginning of each procedure and class.
- * Great to have comments before crucial code sections within the procedure.
-*/
+// assign nav list items.
+function creatNavList() {
+    let counter = 0;
+    for (let item of navList) {
+      const li = document.createElement('li');
+      li.innerHTML = item;
+      li.classList.add('list');
+      li.setAttribute('id', counter++);
+      fragment.appendChild(li);
+    }
+    ul.appendChild(fragment);
+    navigate();
+}
 
-/**
- * Define Global Variables
- * 
-*/
+// make section active relative to the viewport
+function makeActice() {
+    let counter = 0;
+    for(const section of sectionCollection){
+        const li = document.getElementById(counter++);
+        const box = section.getBoundingClientRect();
+        if(box.top <= 150 && box.bottom >= 150){
+            li.classList.add('active');
+        }else{
+            li.classList.remove('active');
+        }
+    }
+}
 
+// navigate to selected section.
+function navigate() {
+    ul.addEventListener('click', function (event) {
+        const name = event.target.nodeName;
+        const id = event.target.id;
+        if(name === 'LI') {
+            sections[id].scrollIntoView({behavior: 'smooth'});
+        }
+    });
+}
+// main function to run the project.
+function main() {
+    creatNavList();
+    document.addEventListener('scroll', function() {
+        makeActice();
+    });
+}
 
-/**
- * End Global Variables
- * Start Helper Functions
- * 
-*/
-
-
-
-/**
- * End Helper Functions
- * Begin Main Functions
- * 
-*/
-
-// build the nav
-
-
-// Add class 'active' to section when near top of viewport
-
-
-// Scroll to anchor ID using scrollTO event
-
-
-/**
- * End Main Functions
- * Begin Events
- * 
-*/
-
-// Build menu 
-
-// Scroll to section on link click
-
-// Set sections as active
-
-
+main();
